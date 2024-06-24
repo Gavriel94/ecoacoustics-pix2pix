ef092df17e466b4f5c5caa99d32852295325938f
Anthony <anthonygavriel@gmail.com>
2024-06-24 15:56:41 +0100

    remove unneeded function

--- audio_analysis.py
+++ audio_analysis.py
@@ -3,42 +3,6 @@ import os
 import pandas as pd
 
 
-def compare_files(directory_path: str, verbose: bool = False):
-    """
-    Analyses all files to determine which are not stero or 24 kHz.
-    """
-    # 24khz sample rate is default
-    CHANNELS = 2
-    SAMPLE_RATE = 24000
-    files = os.listdir(directory_path)
-    # create txt file
-    with open('data/analysis/compare.txt', 'w') as f:
-        pass
-    for file_name in files:
-        if verbose:
-            print('analysing', file_name)
-        path = os.path.join(directory_path, file_name)
-        try:
-            with wave.open(path, 'rb') as w:
-                num_channels = w.getnchannels()
-                sample_rate = w.getframerate()
-                if num_channels != CHANNELS or sample_rate != SAMPLE_RATE:
-                    with open('data/analysis/compare.txt', 'a') as f:
-                        write = (path
-                                 + f' Channels: {num_channels}'
-                                 + f' Sample Rate {sample_rate}'
-                                 + '\n')
-                        f.write(write)
-        except Exception as e:
-            with open('data/analysis/compare.txt', 'a') as f:
-                write = (path
-                         + f' Channels: {num_channels}'
-                         + f' Sample Rate {sample_rate}'
-                         + f' Exception: {str(e)}'
-                         + '\n')
-                f.write(write)
-
-
 def get_dict(num_channels: int, sample_rate: int,
              frames: int, bit_depth: int,
              duration: float, wav_path: str,
@@ -97,21 +61,6 @@ def wav_data(directory_path: str, verbose: bool = False):
                          str(e)))
     return pd.DataFrame(dicts)
 
-
-if not os.path.exists('data/analysis/compare.txt'):
-    # 2023 files
-    compare_files('data/2023_11/SM4/PLI2')
-    compare_files('data/2023_11/SM4/PLI3')
-    compare_files('data/2023_11/SMMicro/PLI2')
-    compare_files('data/2023_11/SMMicro/PLI3')
-
-    # 2024 files
-    compare_files('data/2024_03/SM4/PLI1')
-    compare_files('data/2024_03/SM4/PLI2')
-    compare_files('data/2024_03/SM4/PLI3')
-    compare_files('data/2024_03/SMMicro/PLI1')
-    compare_files('data/2024_03/SMMicro/PLI2')
-    compare_files('data/2024_03/SMMicro/PLI3')
 
 if not os.path.exists('data/analysis/sm4_data.csv') or \
         not os.path.exists('data/analysis/smmicro_data.csv'):
