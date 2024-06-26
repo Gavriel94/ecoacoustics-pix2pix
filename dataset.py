8effcba6e157e646b1d6da7cdb1fdd213eacf87d
Anthony <anthonygavriel@gmail.com>
2024-06-26 11:35:30 +0100

    generated summaries and got a complete dataset

--- dataset.py
+++ dataset.py
@@ -87,23 +87,28 @@ def find_matches(sm4_summary_path: str,
     return matching_times
 
 
-def create_dataset(matched_timestamps: pd.DataFrame, destination: str, mins_offset: int = 0):
+def create_dataset(matched_timestamps: pd.DataFrame,
+                   destination: str,
+                   mins_offset: int = 0,
+                   month_offset: int = 0,
+                   verbose: bool = False):
     """
     Iterates through the df containing matching summaries and creates a list of matching SMMicro
     and SM4 paths. Copies these files to `wav_file_path` to create the dataset.
-    Times in the 2024 file names are 1 minute out of the times listed in the summaries hence the
-    mins_offset parameter.
+    Months or mins are somtimes misaligned from the file names (for example
+    2023_11/SM4/PLI3 contains data from 11/23 and 12/23).
 
     Args:
         matched_timestamps (pd.DataFrame): Df containing the date and times of matching summaries.
         destination (str): Where to save the WAV files.
         mins_offset (int, optional): Align file name with time listed in summary. Defaults to 0.
+        month_offset(int, optional): Align months in the file name to the directory. Defaults to 0.
     """
     def format_time(time: str):
         formatted_time = ''
         hrs, mins, secs = time.split(':')
-        mins = int(mins) - mins_offset
-        formatted_time = formatted_time + hrs + str(mins) + secs
+        mins = str(int(mins) + mins_offset)
+        formatted_time = formatted_time + hrs + mins + secs
         return formatted_time
 
     def get_day(date: str):
@@ -113,6 +118,7 @@ def create_dataset(matched_timestamps: pd.DataFrame, destination: str, mins_offs
     folder, date, mic, location = destination.split('/')
 
     date = date.replace('_', '')
+    date = str(int(date) + month_offset)
     day = [get_day(date) for date in matched_timestamps['DATE']]
     formatted_times = [format_time(time) for time in matched_timestamps['TIME']]
     file_names = []
@@ -126,11 +132,17 @@ def create_dataset(matched_timestamps: pd.DataFrame, destination: str, mins_offs
     for file in file_names:
         file = file + '.wav'
         _, year, _, _, wav_file_name = file.split('/')
-        destination = 'data/dataset/' + year + '/' + location + '/' + wav_file_name
+        dir_path = 'dataset/' + year + '/' + location
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        destination = dir_path + '/' + wav_file_name
         try:
+            if verbose:
+                print(file)
             shutil.copy(file, destination)
         except FileNotFoundError as e:
-            print(str(e))
+            if verbose:
+                print(str(e))
 
 
 pli1 = find_matches('data/2024_03/SM4/summaries/PLI1-4_A_Summary_202403.txt',
@@ -143,32 +155,44 @@ pli3 = find_matches('data/2024_03/SM4/summaries/PLI3-4_A_Summary_202403.txt',
                     'data/2024_03/SMMicro/summaries/PLI3_Summary_20240316_20240424.txt',
                     'data/analysis/summary_matches/2024_03/PLI3.csv')
 
-create_dataset(pli1, 'data/2024_03/SMMicro/PLI1', mins_offset=1)
-create_dataset(pli1, 'data/2024_03/SM4/PLI1', mins_offset=1)
+create_dataset(pli1, 'data/2024_03/SMMicro/PLI1', mins_offset=-1)
+create_dataset(pli1, 'data/2024_03/SM4/PLI1', mins_offset=-1)
 
-create_dataset(pli2, 'data/2024_03/SMMicro/PLI2', mins_offset=1)
-create_dataset(pli2, 'data/2024_03/SM4/PLI2', mins_offset=1)
+create_dataset(pli2, 'data/2024_03/SMMicro/PLI2', mins_offset=-1)
+create_dataset(pli2, 'data/2024_03/SM4/PLI2', mins_offset=-1)
 
-create_dataset(pli3, 'data/2024_03/SMMicro/PLI3', mins_offset=1)
-create_dataset(pli3, 'data/2024_03/SM4/PLI3', mins_offset=1)
+create_dataset(pli3, 'data/2024_03/SMMicro/PLI3', mins_offset=-1)
+create_dataset(pli3, 'data/2024_03/SM4/PLI3', mins_offset=-1)
 
-len_pli1 = len(os.listdir('data/2024_03/SM4/PLI1'))
-len_pli2 = len(os.listdir('data/2024_03/SM4/PLI2'))
-len_pli3 = len(os.listdir('data/2024_03/SM4/PLI3'))
+len_pli1 = len(os.listdir('dataset/2024_03/PLI1'))
+len_pli2 = len(os.listdir('dataset/2024_03/PLI2'))
+len_pli3 = len(os.listdir('dataset/2024_03/PLI3'))
 print(f'2024_03 PLI1 data samples: {len_pli1}')
 print(f'2024_03 PLI2 data samples: {len_pli2}')
 print(f'2024_03 PLI3 data samples: {len_pli3}')
 print('Total 2024 samples:', len_pli1 + len_pli2 + len_pli3)
 
+# generate missing summary files
 create_summary('data/2023_11/SM4/PLI2')
 create_summary('data/2023_11/SM4/PLI3')
 
 pli2_23 = find_matches('data/2023_11/SM4/summaries/PLI2_Summary_20231208_20231224.txt',
                        'data/2023_11/SMMicro/summaries/PLI2_Summary_20231128-231219.txt',
                        'data/analysis/summary_matches/2023_11/PLI2.csv')
+
 pli3_23 = find_matches('data/2023_11/SM4/summaries/PLI3_Summary_20231128_20231223.txt',
                        'data/2023_11/SMMicro/summaries/PLI3_Summary_20231128-231219.txt',
                        'data/analysis/summary_matches/2023_11/PLI3.csv')
-create_dataset(pli2_23, 'data/2023_11/SM4/PLI2')
+
+create_dataset(pli2_23, 'data/2023_11/SM4/PLI2', month_offset=1)
+
+# Half of these files are date 11/23 and half are 12/23
+# running twice with an offset as a 'hacky' solution
 create_dataset(pli3_23, 'data/2023_11/SM4/PLI3')
-# TODO they fucked up the 11/12 in directory and file name
+create_dataset(pli3_23, 'data/2023_11/SM4/PLI3', month_offset=1)
+
+len_pli2_23 = len(os.listdir('dataset/2023_11/PLI2'))
+len_pli3_23 = len(os.listdir('dataset/2023_11/PLI3'))
+print(f'2023_11 PLI2 data samples: {len_pli2}')
+print(f'2023_11 PLI3 data samples: {len_pli3}')
+print('Total 2023 samples:', len_pli2_23 + len_pli3_23)
