5081cea2e51bb7073457267bda47463f1a3c1966
Anthony <anthonygavriel@gmail.com>
2024-07-31 23:49:06 +0100

    needs to be a small batch size as images are large

--- cGAN/config.py
+++ cGAN/config.py
@@ -6,7 +6,7 @@ import cGAN.utilities as utils
 
 DEVICE = utils.set_device('mps')
 LEARNING_RATE = 2e-4
-BATCH_SIZE = 16
-NUM_WORKERS = 2
-NUM_EPOCHS = 2
+BATCH_SIZE = 2
+NUM_WORKERS = 6
+NUM_EPOCHS = 1
 L1_LAMBDA = 100
