ea61c5408bd58ddce15a82fe577840d1338d0d1b
Anthony <anthonygavriel@gmail.com>
2024-07-31 23:50:27 +0100

    use custom l1 loss to ignore pure white

--- cGAN/train.py
+++ cGAN/train.py
@@ -1,43 +1,52 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from tqdm import tqdm
 from . import config as cfg
 
 
-def train_model(discriminator,
-                generator,
-                train_loader,
-                optim_discriminator,
-                optim_generator,
-                l1_loss,
-                bce_w_logits_loss):
-    print('training')
+def custom_l1_loss(input, target, padding_value=0):
+    if input.size() != target.size():
+        target = F.interpolate(target, size=input.size()[2:], mode='bilinear', align_corners=False)
+
+    mask = (input != padding_value).float()
+    loss = nn.L1Loss(reduction='none')(input, target)
+    masked_loss = loss * mask
+    return masked_loss.sum() / mask.sum()
+
+
+def train_model(disc, gen, train_loader, optim_disc, optim_gen, l1_loss, bce_logits):
     for epoch in range(cfg.NUM_EPOCHS):
-        discriminator.train()
-        generator.train()
-        loop = tqdm(train_loader, leave=True)
-        for idx, (x, y) in enumerate(loop):
-            x, y = x.to(cfg.DEVICE), y.to(cfg.DEVICE)
+        disc.train()
+        gen.train()
+        train_loader_tqdm = tqdm(train_loader, leave=True)
+        for idx, (input_img, target_img) in enumerate(train_loader_tqdm):
+            input_img, target_img = input_img.to(cfg.DEVICE), target_img.to(cfg.DEVICE)
 
             # train discriminator
-            with torch.autocast(device_type=cfg.DEVICE.type):
-                y_fake = generator(x)
-                D_real = discriminator(x, y)
-                D_fake = discriminator(x, y_fake.detach())
-                D_real_loss = bce_w_logits_loss(D_real, torch.ones_like(D_real))
-                D_fake_loss = bce_w_logits_loss(D_fake, torch.zeros_like(D_fake))
-                D_loss = (D_real_loss + D_fake_loss) / 2
+            y_fake = gen(input_img)
+            D_real = disc(input_img, target_img)
+            D_fake = disc(input_img, y_fake.detach())
+            D_real_loss = bce_logits(D_real, torch.ones_like(D_real))
+            D_fake_loss = bce_logits(D_fake, torch.zeros_like(D_fake))
+            D_loss = (D_real_loss + D_fake_loss) / 2
 
-            optim_discriminator.zero_grad()
+            optim_disc.zero_grad()
             D_loss.backward()
-            optim_discriminator.step()
+            optim_disc.step()
 
             # train generator
-            with torch.autocast(device_type=cfg.DEVICE.type):
-                D_fake = discriminator(x, y_fake)
-                G_fake_loss = bce_w_logits_loss(D_fake, torch.ones_like(D_fake))
-                L1 = l1_loss(y_fake, y) * cfg.L1_LAMBDA
-                G_loss = G_fake_loss + L1
+            D_fake = disc(input_img, y_fake)
+            G_fake_loss = bce_logits(D_fake, torch.ones_like(D_fake))
+            L1 = custom_l1_loss(y_fake, target_img) * cfg.L1_LAMBDA
+            G_loss = G_fake_loss + L1
 
-            optim_generator.zero_grad()
+            optim_gen.zero_grad()
             G_loss.backward()
-            optim_generator.step()
+            optim_gen.step()
+
+            train_loader_tqdm.set_postfix({
+                'D_loss': D_loss.item(),
+                'G_loss': G_loss.item(),
+                'L1_loss': L1.item()
+            })
