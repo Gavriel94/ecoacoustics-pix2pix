01711e121df8f703cdc1f3473a936e860ce1f42b
Anthony <anthonygavriel@gmail.com>
2024-08-08 09:22:39 +0100

    minimise

--- script.py
+++ script.py
@@ -3,75 +3,105 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from pix2pix.dataset import SpectrogramDataset
+from pix2pix.ds import Pix2PixDataset
 from pix2pix.discriminator import Discriminator
 from pix2pix.generator import Generator
 from dataset import create_dataset
 from pix2pix.train import train_model
-from pix2pix import config as cfg
 import pix2pix.utilities as utils
-from pix2pix.ds import Pix2PixDataset
+# import logging
+
+
+# def setup_logging():
+#     logging.basicConfig(format='%(asctime)s:%(message)s',
+#                         level=logging.INFO,
+#                         datefmt='%m/%d/%Y %I:%M:%S %p')
+#     logging.getLogger('PIL').setLevel(logging.WARNING)
+
+RAW_DATA = 'raw_data_test'
+DATASET = 'data'
+DATASET_PATH = os.path.join(DATASET, 'dataset')
+
+MATCHED_SUMMARIES = [['data/2024_03/PLI1/summary.csv',
+                      'data/2024_03/PLI2/summary.csv',
+                      'data/2024_03/PLI3/summary.csv'],
+                     ['data/2023_11/PLI2/summary.csv',
+                      'data/2023_11/PLI3/summary.csv']]
+
+COPIED_RECORDINGS = [['data/2024_03/PLI1',
+                      'data/2024_03/PLI2',
+                      'data/2024_03/PLI3'],
+                     ['data/2023_11/PLI2',
+                      'data/2023_11/PLI3']]
+
+SPECTROGRAM_PATHS = [['data/spectrograms/2024_03/PLI2',
+                      'data/spectrograms/2024_03/PLI3',
+                      'data/spectrograms/2024_03/PLI1'],
+                     ['data/spectrograms/2023_11/PLI2',
+                      'data/spectrograms/2023_11/PLI3']]
+
+# Model config parameters
+DEVICE = utils.set_device('mps')
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 2
+NUM_WORKERS = 6
+NUM_EPOCHS = 2
+L1_LAMBDA = 100
 
 
 def main():
-    create_new_dataset = False
+    create_new_dataset = True
+    train_model = True
 
-    if create_new_dataset or not os.path.isdir(cfg.DATASET_PATH) or len(os.listdir(cfg.DATASET_PATH)) == 0:
-        print(cfg.DATASET_PATH)
-        print('Creating a new dataset in \'data/\'')
-        create_dataset(data_root=cfg.DATA_ROOT,
-                       dataset_root=cfg.DATASET_ROOT,
+    if create_new_dataset:
+        print(f'Creating {DATASET_PATH}\n')
+        create_dataset(data_root=RAW_DATA,
+                       dataset_root=DATASET,
                        analysis=False,
-                       matched_summaries=cfg.MATCHED_SUMMARIES,
-                       copied_recordings=cfg.COPIED_RECORDINGS,
-                       spectrogram_paths=cfg.SPECTROGRAM_PATHS,
+                    #    matched_summaries=MATCHED_SUMMARIES,
+                    #    copied_recordings=COPIED_RECORDINGS,
+                    #    spectrogram_paths=SPECTROGRAM_PATHS,
                        verbose=True)
+    if train_model:
+        # get files from dataset
+        files = utils.get_files(DATASET_PATH, include_correlated=False)
+
+        # * used for debugging - alter the size of dataset
+        files = files[:10]
+
+        train, val, test = utils.split_data(files, 0.8)
+
+        train_dataset = Pix2PixDataset(train, augment=False)
+        
+        train_loader = DataLoader(train_dataset,
+                                  batch_size=BATCH_SIZE,
+                                  num_workers=NUM_WORKERS,
+                                  shuffle=True,
+                                  collate_fn=utils.custom_collate)
+
+        disc = Discriminator(in_ch=1).to(DEVICE)
+        gen = Generator(in_ch=1, features=64).to(DEVICE)
+        # setting betas reduce momentum (used in the paper)
+        optim_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+        optim_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+        BCE = nn.BCEWithLogitsLoss()
+        L1_LOSS = nn.L1Loss()
+        disc_loss, gen_loss, l1_loss = train_model(disc, gen, train_loader,
+                                                   optim_disc, optim_gen, L1_LOSS, L1_LAMBDA,
+                                                   BCE, NUM_EPOCHS, DEVICE)
+        utils.plot_loss(disc_loss, gen_loss, l1_loss)
+        
+    spectrogram_name = 'PLI1-4_202316_121100'
     
-    # get files from dataset
-    files = utils.get_files(cfg.DATASET_PATH, include_correlated=False)
-
-    # * used for debugging - alter the size of dataset
-    files = files[:10]
-
-    # view dataset files
-    # for file_path in files:
-    #     file = Image.open(file_path)
-    #     save_path = file_path.split('/')[2]  # includes .png
-    #     file.save(f'tmp/{save_path}')
-
-    train, val, test = utils.split_data(files, 0.8)
-
-    train_dataset = Pix2PixDataset(train, augment=False)
-    val_dataset = Pix2PixDataset(val, augment=False)
-    test_dataset = Pix2PixDataset(test, augment=False)
+    # gets true recording
+    location, date, time = spectrogram_name.split('_')
+    location = location.replace('-4', '')
+    date = date[:4] + '_' + date[4:]
+    true_recording_path = os.path.join('data', date, location, spectrogram_name)
     
-    train_loader = DataLoader(train_dataset,
-                              batch_size=cfg.BATCH_SIZE,
-                              num_workers=cfg.NUM_WORKERS,
-                              shuffle=True,
-                              collate_fn=utils.custom_collate)
-    val_loader = DataLoader(val_dataset,
-                            batch_size=cfg.BATCH_SIZE,
-                            num_workers=cfg.NUM_WORKERS,
-                            shuffle=False,
-                            collate_fn=utils.custom_collate)
-    test_loader = DataLoader(test_dataset,
-                             batch_size=cfg.BATCH_SIZE,
-                             num_workers=cfg.NUM_WORKERS,
-                             shuffle=False,
-                             collate_fn=utils.custom_collate)
-
-    disc = Discriminator(in_ch=1).to(cfg.DEVICE)
-    gen = Generator(in_ch=1, features=64).to(cfg.DEVICE)
-    # setting betas reduce momentum (used in the paper)
-    optim_disc = optim.Adam(disc.parameters(), lr=cfg.LEARNING_RATE, betas=(0.5, 0.999))
-    optim_gen = optim.Adam(gen.parameters(), lr=cfg.LEARNING_RATE, betas=(0.5, 0.999))
-    BCE = nn.BCEWithLogitsLoss()
-    L1_LOSS = nn.L1Loss()
-    disc_loss, gen_loss, l1_loss = train_model(disc, gen, train_loader,
-                                               optim_disc, optim_gen, L1_LOSS,
-                                               BCE)
-    utils.plot_loss(disc_loss, gen_loss, l1_loss)
+    # data/year/location/spectrogram_name
+    
+    # utils.spectrogram_to_audio(f'{DATASET_ROOT}/', f'{DATASET_ROOT}spectrograms/2024_03/PLI1/params/{spectrogram}.json', )
 
 
 if __name__ == '__main__':
