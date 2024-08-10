c01af9ad4e5be7314c70bd87f9cd007969ed1119
Anthony <anthonygavriel@gmail.com>
2024-08-10 22:49:00 +0100

    replace batchnorm with instantnorm, return global avg pooling

--- pix2pix/discriminator.py
+++ pix2pix/discriminator.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 
 
-class CNNBlock(nn.Module):
+class ConvBlock(nn.Module):
     def __init__(self, in_ch: int, out_ch: int, stride: int):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 4, stride, bias=False, padding_mode='reflect'),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv2d(in_ch, out_ch, 4, stride, padding=1, bias=False, padding_mode='reflect'),
+            nn.InstanceNorm2d(out_ch, affine=True),
             nn.LeakyReLU(0.2)
         )
 
@@ -30,17 +30,19 @@ class Discriminator(nn.Module):
 
         layers = []
         in_ch = features[0]
-        for feature in features[1:]:
-            stride = 1 if feature == features[-1] else 2
-            layers.append(CNNBlock(in_ch, feature, stride=stride))
+        for idx, feature in enumerate(features[1:]):
+            layers.append(ConvBlock(in_ch,
+                                    feature,
+                                    stride=1 if feature == features[-1] else 2))
             in_ch = feature
         layers.append(
             nn.Conv2d(in_ch, 1, kernel_size=4, stride=1, padding=1, padding_mode='reflect')
         )
         self.model = nn.Sequential(*layers)
+        self.gap = nn.AdaptiveAvgPool2d(1)  # global average pooling
 
     def forward(self, x, y):
         x = torch.cat([x, y], dim=1)
         x = self.input(x)
-        x = self.model(x)
-        return x
+        features = self.model(x)
+        return self.gap(features).view(x.size(0), -1), features
