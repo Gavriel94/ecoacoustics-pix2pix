6663abdf3c577a1f21646d85efb94ccf2adbcca3
Anthony <anthonygavriel@gmail.com>
2024-08-10 17:39:59 +0100

    make custom loss its own module

--- pix2pix/train.py
+++ pix2pix/train.py
@@ -1,27 +1,15 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from tqdm import tqdm
 import os
 
 from . import utilities as ut
 
 
-def custom_l1_loss(input, target, padding_value=1.0):
-    if input.size() != target.size():
-        target = F.interpolate(target, size=input.size()[2:], mode='bilinear', align_corners=False)
-
-    mask = (input != padding_value).float()
-    loss = nn.L1Loss(reduction='none')(input, target)
-    masked_loss = loss * mask
-    return masked_loss.sum() / mask.sum()
-
-
-def train(discriminator, generator, data_loader, optim_discriminator, optim_generator, l1_loss, l1_lambda, bce_logits, num_epochs, device, save_dir: str, accumulation_steps: int = 1, display_epoch: int = 5):
+def train(discriminator, generator, data_loader, optim_discriminator, optim_generator, L1_loss, l1_lambda, bce_logits, num_epochs, device, save_dir: str, accumulation_steps: int = 1, display_epoch: int = 5):
     os.makedirs(f'{save_dir}/evaluation', exist_ok=True)
     run_name = f"{save_dir}/evaluation/run_{len(os.listdir(f'{save_dir}/evaluation')) + 1}"
     os.makedirs(run_name, exist_ok=True)
-    disc_loss, gen_loss, l1_loss = [], [], []
+    disc_losses, gen_losses, l1_losses = [], [], []
     for epoch in range(num_epochs):
         discriminator.train()
         generator.train()
@@ -46,7 +34,7 @@ def train(discriminator, generator, data_loader, optim_discriminator, optim_gene
             # train generator
             D_fake = discriminator(input_img, generated_img)  # disc output for synth pair (input, generated)
             G_fake_loss = bce_logits(D_fake, torch.ones_like(D_fake))  # BCE for synth images (gen tries to fool dis, should be close to 1)
-            L1 = custom_l1_loss(generated_img, target_img) * l1_lambda # L1 loss between synth and target images encourage visual similarity
+            L1 = L1_loss(generated_img, target_img) * l1_lambda # L1 loss between synth and target images encourage visual similarity
             G_loss = G_fake_loss + L1  # generator loss is a combination of adverserial (G_fake_loss) and L1 loss
             # backpropagate and update generator weights
             G_loss_norm = G_loss / accumulation_steps
@@ -57,9 +45,9 @@ def train(discriminator, generator, data_loader, optim_discriminator, optim_gene
                 'G_loss': G_loss.item(),
                 'L1_loss': L1.item()
             })
-            disc_loss.append(D_loss.item())
-            gen_loss.append(G_loss.item())
-            l1_loss.append(L1.item())
+            disc_losses.append(D_loss.item())
+            gen_losses.append(G_loss.item())
+            l1_losses.append(L1.item())
 
             if (idx + 1) % accumulation_steps == 0:
                 optim_discriminator.step()
@@ -68,7 +56,6 @@ def train(discriminator, generator, data_loader, optim_discriminator, optim_gene
                 optim_generator.zero_grad()
 
             if idx % display_epoch == 0:
-                save_path = f'{run_name}/'
                 # os.makedirs(save_path, exist_ok=True)
                 img_name = str(img_name[idx])  # unpack from tuple
 
@@ -96,4 +83,39 @@ def train(discriminator, generator, data_loader, optim_discriminator, optim_gene
             optim_discriminator.zero_grad()
             optim_generator.zero_grad()
 
-    ut.plot_loss(disc_loss, gen_loss, l1_loss, run_name)
+    # make directory for metrics captured during training
+    graphs_path = os.path.join(run_name, 'graphs')
+    os.makedirs(graphs_path, exist_ok=True)
+
+    # plot and save discriminator loss
+    disc_path = os.path.join(graphs_path, 'disc_loss')
+    ut.save_figure(disc_losses,
+                   title='Discriminator Loss',
+                   xlabel='Epoch',
+                   ylabel='Loss',
+                   save_path=disc_path)
+
+    # plot and save generator loss
+    gen_path = os.path.join(graphs_path, 'gen_loss')
+    ut.save_figure(gen_losses,
+                   title='Generator Loss',
+                   xlabel='Epoch',
+                   ylabel='Loss',
+                   save_path=gen_path)
+
+    # compare discriminator and generator loss
+    disc_gen_path = os.path.join(graphs_path, 'disc_gen_loss')
+    ut.save_figure(disc_losses,
+                   gen_losses,
+                   title='Discriminator and Generator Loss',
+                   xlabel='Epoch',
+                   ylabel='Loss',
+                   save_path=disc_gen_path)
+
+    # plot and save L1 loss
+    l1_path = os.path.join(graphs_path, 'l1_loss')
+    ut.save_figure(l1_losses,
+                   title='L1 Loss',
+                   xlabel='Epoch',
+                   ylabel='Loss',
+                   save_path=l1_path)
