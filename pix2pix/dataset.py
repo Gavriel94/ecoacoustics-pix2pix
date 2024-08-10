3c2f54c4313c17a380968c734fe12c343d9a25f6
Anthony <anthonygavriel@gmail.com>
2024-08-10 12:34:07 +0100

    begin documenting

--- pix2pix/dataset.py
+++ pix2pix/dataset.py
@@ -8,10 +8,30 @@ import os
 
 
 class Pix2PixDataset(Dataset):
-    def __init__(self, dataset: list, use_correlated: bool, augment: bool):
-        
-        # data is a list of paths like data/train/training_spectrogram.png
-        self.data = [os.path.join(dataset, file) for file in os.listdir(dataset) if file.endswith('.png')]
+    """
+    Dataset for a Pix2Pix cGAN.
+
+    The data is a series of input and target images sharing a single canvas.
+    An image is padded until its dimensions are a value in the series 2^n.
+    The padding image is split in the middle to separate the input and target image.
+
+    Args:
+        Dataset (torch.utils.data): An abstract class representing a Dataset.
+    """
+    def __init__(self, dataset: list, use_correlated: bool):
+        """
+        Initialise a Pix2Pix Dataset.
+
+        Args:
+            dataset (list): List of paths of stitched spectrogam image files.
+            use_correlated (bool): Use images marked as correlated.
+
+        Raises:
+            Exception: No correlated folder, or correlated folder is empty.
+        """
+        self.data = [os.path.join(dataset, file)
+                     for file in os.listdir(dataset) if file.endswith('.png')]
+
         if use_correlated:
             if 'correlated' in os.listdir(dataset):
                 if len(os.listdir(os.path.join(dataset, 'correlated'))) == 0:
@@ -19,29 +39,18 @@ class Pix2PixDataset(Dataset):
                 for file in os.listdir(os.path.join(dataset, 'correlated')):
                     self.data.append(os.path.join(dataset, 'correlated', file))
             else:
-                raise FileNotFoundError('No correlated directory.')
-        
-        self.augmentation = v2.Compose([
-            v2.Resize((224, 224)),
-            v2.Pad(padding=4, fill=(0, 0, 0), padding_mode='constant'),
-            v2.ToImage(),
-            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            v2.ToDtype(torch.float32, scale=True)
-        ])
-        self.normalise = v2.Compose([
-            v2.ToImage(),
-            # v2.Normalize(mean=[0.5], std=[0.5])
-        ])
+                raise Exception('No correlated directory.')
+
         self.to_tensor = v2.Compose([
             v2.ToImage(),
-            # v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            v2.ToDtype(torch.float32, scale=True)
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5], std=[0.5])
         ])
 
     def calculate_padding_dimensions(self, img_shape):
         def next_power_of_2(x):
             return 2 ** math.ceil(math.log2(x))
-        
+
         width, height = img_shape
         target_width = max(next_power_of_2(width), width)
         target_height = max(next_power_of_2(height), height)
@@ -56,7 +65,7 @@ class Pix2PixDataset(Dataset):
         pad_right = pad_width - pad_left
         pad_top = pad_height // 2
         pad_bottom = pad_height - pad_top
-        
+
         padding_coords = {
             'left': pad_left,
             'right': pad_right,
@@ -75,8 +84,7 @@ class Pix2PixDataset(Dataset):
 
     def __getitem__(self, idx):
         image_path = self.data[idx]
-        
-        print('Retrieving image', image_path)
+
         image = Image.open(image_path).convert('L')
         image_arr = np.array(image)
 
@@ -92,10 +100,10 @@ class Pix2PixDataset(Dataset):
         input_tensor = self.to_tensor(padded_input)
         target_tensor = self.to_tensor(padded_target)
         original_size = image_arr.shape
-        
-        _, _, image_file = image_path.split('/')
 
-        return input_tensor, target_tensor, original_size, padding_coords, image_file
+        _, _, image_name = image_path.split('/')
+
+        return input_tensor, target_tensor, original_size, padding_coords, image_name
 
 
 class NotTwoPower(Exception):
