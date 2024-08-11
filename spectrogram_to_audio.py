fda0f23af39db8a9f903a951682e3a0ab59242dc
Anthony <anthonygavriel@gmail.com>
2024-08-11 23:21:32 +0100

    prelim work into spec to audio

--- spectrogram_to_audio.py
+++ spectrogram_to_audio.py
@@ -1,24 +1,34 @@
 """
-Script to turn a spectrogram into audio using a Pix2Pix utility function.
+Script to turn spectrograms from the test set into audio using a Pix2Pix utility function.
+Used to evaluate the cGANs synthesised spectrograms with unseen data.
 """
-
-import os
 from pix2pix import utilities as utils
+import config
 
 
 def main():
-    spectrogram_path = 'data/spectrograms/PLI1_20240316_115600.png'
-    filename = spectrogram_path.split('/')[2].replace('.png', '.wav')
+    raw_data = config.RAW_DATA_ROOT
+    data = config.DATASET_ROOT
 
-    output_path = os.path.join('data', 'spectrograms', 'spectrogram_to_audio')
-    os.makedirs(output_path, exist_ok=True)
+    target_mic = 'SM4'
+    target_mic_delim = '-4'
 
-    overwrite = True
-    if not overwrite:
-        if os.path.exists(os.path.join(output_path, filename)):
-            raise FileExistsError
+    test_samples = utils.get_test_sample(dataset_root=data,
+                                         raw_data_root=raw_data,
+                                         mic2_name=target_mic,
+                                         mic2_delim=target_mic_delim,
+                                         num_samples=3)
+    print(test_samples)
 
-    utils.spectrogram_to_audio(spectrogram_path, output_path, sample_rate=48000)
+    # output_path = os.path.join('data', 'spectrograms', 'spectrogram_to_audio')
+    # os.makedirs(output_path, exist_ok=True)
+
+    # overwrite = True
+    # if not overwrite:
+    #     if os.path.exists(os.path.join(output_path, filename)):
+    #         raise FileExistsError
+
+    # utils.spectrogram_to_audio(spectrogram_path, output_path, sample_rate=48000)
 
 
 if __name__ == '__main__':
