64785b177a691e1da66372dcff754c0bb98c4ecd
Anthony <anthonygavriel@gmail.com>
2024-08-11 19:10:23 +0100

    document and tidy

--- pix2pix/train.py
+++ pix2pix/train.py
@@ -1,31 +1,74 @@
-import torch
-from tqdm import tqdm
+"""
+Train the spectrogram translation conditional Generative Adversial Network.
+"""
+
 import os
-from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+
+import torch
+from torchmetrics.image import (PeakSignalNoiseRatio,
+                                StructuralSimilarityIndexMeasure)
+from tqdm import tqdm
 
 from . import utilities as ut
 
 
-def train(discriminator, generator, train_loader, validation_loader, optim_discriminator, optim_generator, L1_loss, l1_lambda, bce_logits, num_epochs, device, save_dir: str, accumulation_steps: int = 1, display_epoch: int = 5):
+def train(discriminator, generator,
+          train_loader, validation_loader,
+          optim_discriminator, optim_generator,
+          custom_loss, loss_lambda,
+          bce_logits, num_epochs,
+          device, save_dir: str,
+          accumulation_steps, view_val_epoch: int = 5):
+    """
+    Train the conditional Generative Adverserial Network (cGAN).
+
+    Evaluates the model at the end of each batch by running the cGAN on
+    unseen data. Data used in the evaluation does not contribute to weight
+    updates in the model.
+
+    Pairs of generated and target images are saved during training so
+    the quality of the output can be inspected in real time.
+
+    An average of both peak signal to noise ratio (PSNR) and
+    structural similarity index measure (SSIM) are computed to quantify
+    how similar the synthesised image is to the target image.
+
+    Args:
+        discriminator (Discriminator): Discriminator model.
+        generator (Generator): Generator model.
+        train_loader (DataLoader): Training data.
+        validation_loader (DataLoader): Validation data.
+        optim_discriminator (optim.Adam): Discriminator optimiser.
+        optim_generator (optim.Adam): Discriminator generator.
+        custom_loss (Pix2PixLoss): Generator loss function.
+        loss_lambda (int): Balance combined L1/Intensity with adverserial loss.
+        bce_logits (nn.BCEWithLogitsLoss): Discriminator loss function.
+        num_epochs (int): Number of training epochs.
+        device (torch.device): Device for tensor operations.
+        save_dir (str): Output for evaluation data.
+        accumulation_steps (int): Steps before updating gradients. Simulates batching.
+        view_val_epoch (int, optional): View attempts on validation data. Defaults to 5.
+    """
     os.makedirs(f'{save_dir}/evaluation', exist_ok=True)
     num_runs = len(os.listdir(f'{save_dir}/evaluation'))
     run_name = f"{save_dir}/evaluation/run_{num_runs + 1}"
     os.makedirs(run_name, exist_ok=True)
+
     disc_losses, gen_losses, l1_losses = [], [], []
     val_psnr_scores, val_ssim_scores = [], []
-
     psnr = PeakSignalNoiseRatio().to(device)
     ssim = StructuralSimilarityIndexMeasure().to(device)
+
     print(f'Run {num_runs + 1}')
     for epoch in range(num_epochs):
         print(f'Epoch {epoch + 1}')
         discriminator.train()
         generator.train()
-        data_loader_tqdm = tqdm(train_loader, leave=True)
+        data_loader_tqdm = tqdm(train_loader, leave=True)  # view progress bar while training
         for idx, (input_img, target_img, original_size, padding_coords, img_names) in enumerate(data_loader_tqdm):
             input_img, target_img = input_img.to(device), target_img.to(device)
 
-            # train discriminator
+            # * train discriminator
             generated_img = generator(input_img)
             D_real, real_features = discriminator(input_img, target_img)
             D_fake, fake_features = discriminator(input_img, generated_img.detach())
@@ -34,16 +77,16 @@ def train(discriminator, generator, train_loader, validation_loader, optim_discr
             D_fake_loss = bce_logits(D_fake, torch.zeros_like(D_fake))
             # calculate average of losses
             D_loss = (D_real_loss + D_fake_loss) / 2
-            # normalise loss based on accumulation steps
+            # normalise loss based on accumulation steps (simulate batching)
             D_loss_norm = D_loss / accumulation_steps
             # backpropagate
             D_loss_norm.backward()
 
             # train generator
-            D_fake, fake_features = discriminator(input_img, generated_img)  # disc output for synth pair (input, generated)
-            G_fake_loss = bce_logits(D_fake, torch.ones_like(D_fake))  # BCE for synth images (gen tries to fool dis, should be close to 1)
-            L1 = L1_loss(generated_img, target_img) * l1_lambda # L1 loss between synth and target images encourage visual similarity
-            G_loss = G_fake_loss + L1  # generator loss is a combination of adverserial (G_fake_loss) and L1 loss
+            D_fake, fake_features = discriminator(input_img, generated_img)
+            G_fake_loss = bce_logits(D_fake, torch.ones_like(D_fake))  # BCE for synth images
+            l1_intensity_aware_loss = custom_loss(generated_img, target_img) * loss_lambda
+            G_loss = G_fake_loss + l1_intensity_aware_loss
             # backpropagate and update generator weights
             G_loss_norm = G_loss / accumulation_steps
             G_loss_norm.backward()
@@ -52,13 +95,13 @@ def train(discriminator, generator, train_loader, validation_loader, optim_discr
             data_loader_tqdm.set_postfix({
                 'D_loss': D_loss.item(),
                 'G_loss': G_loss.item(),
-                'L1_loss': L1.item()
+                'Loss': l1_intensity_aware_loss.item()
             })
 
             # save metrics
             disc_losses.append(D_loss.item())
             gen_losses.append(G_loss.item())
-            l1_losses.append(L1.item())
+            l1_losses.append(l1_intensity_aware_loss.item())
 
             # update weights with accumulated gradients
             if (idx + 1) % accumulation_steps == 0:
@@ -67,31 +110,31 @@ def train(discriminator, generator, train_loader, validation_loader, optim_discr
                 optim_discriminator.zero_grad()
                 optim_generator.zero_grad()
 
-            # save images to disk for inspection
-            if idx % display_epoch == 0:
+        # save images to disk for inspection
+        if idx % view_val_epoch == 0:
+            batch_size = input_img.size(0)
+            for val_idx in range(batch_size):
+                # os.makedirs(save_path, exist_ok=True)
                 batch_size = input_img.size(0)
                 for val_idx in range(batch_size):
-                    # os.makedirs(save_path, exist_ok=True)
-                    batch_size = input_img.size(0)
-                    for val_idx in range(batch_size):
-                        img_name = img_names[val_idx]
+                    img_name = img_names[val_idx]
 
-                    # crop and save input image
-                    input_cropped = ut.remove_padding(input_img[val_idx], original_size,
-                                                      padding_coords, is_target=False)
-                    input_path = os.path.join(run_name, f'e{epoch}_b{idx}_i_{img_name}')
-                    ut.save_tensor_as_img(input_cropped, input_path)
+                # crop and save input image
+                input_cropped = ut.remove_padding(input_img[val_idx], original_size,
+                                                    padding_coords, is_target=False)
+                input_path = os.path.join(run_name, f'e{epoch}_b{idx}_i_{img_name}')
+                ut.save_tensor_as_img(input_cropped, input_path)
 
-                    # crop and save target image
-                    target_cropped = ut.remove_padding(target_img[val_idx], original_size,
-                                                       padding_coords, is_target=True)
-                    target_path = os.path.join(run_name, f'e{epoch}_b{idx}_t_{img_name}')
-                    ut.save_tensor_as_img(target_cropped, target_path)
+                # crop and save target image
+                target_cropped = ut.remove_padding(target_img[val_idx], original_size,
+                                                    padding_coords, is_target=True)
+                target_path = os.path.join(run_name, f'e{epoch}_b{idx}_t_{img_name}')
+                ut.save_tensor_as_img(target_cropped, target_path)
 
-                    generated_cropped = ut.remove_padding(generated_img[val_idx], original_size,
-                                                          padding_coords, is_target=False)
-                    generated_path = os.path.join(run_name, f'e{epoch}_b{idx}_g_{img_name}')
-                    ut.save_tensor_as_img(generated_cropped, generated_path)
+                generated_cropped = ut.remove_padding(generated_img[val_idx], original_size,
+                                                        padding_coords, is_target=False)
+                generated_path = os.path.join(run_name, f'e{epoch}_b{idx}_g_{img_name}')
+                ut.save_tensor_as_img(generated_cropped, generated_path)
 
         # validation step
         generator.eval()
@@ -100,22 +143,25 @@ def train(discriminator, generator, train_loader, validation_loader, optim_discr
         num_val_batches = 0
 
         with torch.no_grad():
+            # get a batch of validation data
             for val_idx, (val_input, val_target, val_size, val_padding_coords, val_name) in enumerate(validation_loader):
                 val_input, val_target = val_input.to(device), val_target.to(device)
                 val_generated = generator(val_input)
 
+                # get psnr and ssim by comparing generated and target images
                 val_psnr += psnr(val_generated, val_target)
                 val_ssim += ssim(val_generated, val_target)
                 num_val_batches += 1
 
-            if idx % display_epoch == 0:
+            if idx % view_val_epoch == 0:
                 val_dir = os.path.join(run_name, 'validation')
                 os.makedirs(val_dir, exist_ok=True)
                 for batch_idx in range(min(3, val_input.size(0))):
-                    val_input_cropped = ut.remove_padding(val_input[batch_idx], val_size,
-                                                          val_padding_coords, is_target=False)
-                    val_input_path = os.path.join(val_dir, f'e{epoch}_b{batch_idx}_i_{str(val_name[batch_idx])}')
-                    ut.save_tensor_as_img(val_input_cropped, val_input_path)
+                    # uncomment to save input images as well
+                    # val_input_cropped = ut.remove_padding(val_input[batch_idx], val_size,
+                    #                                       val_padding_coords, is_target=False)
+                    # val_input_path = os.path.join(val_dir, f'e{epoch}_b{batch_idx}_i_{str(val_name[batch_idx])}')
+                    # ut.save_tensor_as_img(val_input_cropped, val_input_path)
 
                     val_target_cropped = ut.remove_padding(val_target[batch_idx], val_size,
                                                            val_padding_coords, is_target=True)
@@ -127,13 +173,14 @@ def train(discriminator, generator, train_loader, validation_loader, optim_discr
                     val_generated_path = os.path.join(val_dir, f'e{epoch}_b{batch_idx}_g_{str(val_name[batch_idx])}')
                     ut.save_tensor_as_img(val_generated_cropped, val_generated_path)
 
+        # store average psnr and ssim
         avg_val_psnr = val_psnr / num_val_batches
         avg_val_ssim = val_ssim / num_val_batches
         val_psnr_scores.append(avg_val_psnr.item())
         val_ssim_scores.append(avg_val_ssim.item())
 
         print(f'Validation PSNR: {avg_val_psnr:.4f}')  # 20-40 is best
-        print(f'Validation SSIM: {avg_val_ssim:.4f}')  # ranges between -1 to 1 with 1 being best
+        print(f'Validation SSIM: {avg_val_ssim:.4f}')  # ranges between -1 to 1 with 1 'most similar'
         print()
 
         # final update if number of batches is not divisible by accumulation_steps
--- pix2pix/utilities.py
+++ pix2pix/utilities.py
@@ -1,152 +1,26 @@
 """
-Utility functions
+Model utility functions.
 
+- set_device
+- get_files
+- train_val_test_split
+- custom_collate
+- spectrogram_to_audio
+- remove_padding
+- save_tensor_as_img
+- save_figure
 """
 import gzip
+import json
 import os
 import random
+
 import librosa
-import torch
 import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
-import torch.nn.functional as F
-import torch.nn as nn
-import json
 import soundfile as sf
-
-
-def spectrogram_to_audio(spectrogram_path: str, output_path: str, sample_rate):
-    # open spectrogram
-    spectrogram_img = Image.open(spectrogram_path)
-    spectrogram_arr = np.array(spectrogram_img)
-
-    # get magnitude and phase values
-    root, specs, img_path = spectrogram_path.split('/')
-    params_path = os.path.join(root, specs, 'params', img_path.replace('.png', '.json'))
-    # with open(params_path, 'r') as f:
-    with gzip.open(params_path, 'wt') as f:  # jsons are zipped
-        params = json.load(f)
-        magnitude_real = np.array(params['magnitude_real'])
-        magnitude_imag = np.array(params['magnitude_imag'])
-        phase_real = np.array(params['phase_real'])
-        phase_imag = np.array(params['phase_imag'])
-
-        # recreate complex numbers
-        magnitude = magnitude_real + 1j * magnitude_imag
-        phase = phase_real + 1j * phase_imag
-
-        s_db = (spectrogram_arr / 255) * (np.max(magnitude) - np.min(magnitude)) + np.min(magnitude)
-        s = librosa.db_to_amplitude(s_db, ref=np.max(magnitude))
-
-        # shift sine waves
-        stft_matrix = s * phase
-
-        # inverse short term fourier transform
-        y = librosa.istft(stft_matrix,
-                          hop_length=params['hop_length'],
-                          win_length=params['n_fft'],
-                          length=params.get('original_length'))
-
-        file_path = os.path.join(output_path, spectrogram_path.split('/')[2].replace('.png', '.wav'))
-        # save audio
-        sf.write(file_path, y, sample_rate)
-
-
-def remove_padding(tensor, original_dimensions, pad_coords: dict, is_target):
-    try:
-        batch_size, _, h, w = tensor.shape
-        for i in range(batch_size):
-            orig_h, orig_w = original_dimensions[i]
-            pad_coords = pad_coords[i]
-            if is_target:
-                cropped_tensor = tensor[i:i+1, :, pad_coords['top']:pad_coords['top'] + orig_h, :w - pad_coords['right']]
-            else:
-                cropped_tensor = tensor[i:i+1, :, pad_coords['top']:pad_coords['top'] + orig_h, pad_coords['left']:pad_coords['left'] + orig_w]
-
-            return cropped_tensor
-    except ValueError:
-        batch_size, h, w = tensor.shape
-        for i in range(batch_size):
-            orig_h, orig_w = original_dimensions[i]
-            pad_coords = pad_coords[i]
-            if is_target:
-                cropped_tensor = tensor[i:i+1, pad_coords['top']:pad_coords['top'] + orig_h, :w - pad_coords['right']]
-            else:
-                cropped_tensor = tensor[i:i+1, pad_coords['top']:pad_coords['top'] + orig_h, pad_coords['left']:pad_coords['left'] + orig_w]
-
-            return cropped_tensor
-
-
-def custom_collate(batch):
-    input_tensors, target_tensors, original_dimensions, padding_coords, image_path = zip(*batch)
-    return torch.stack(input_tensors), torch.stack(target_tensors), original_dimensions, padding_coords, image_path
-
-
-def save_tensor_as_img(tensor, save_path):
-    t = tensor.cpu().detach().numpy()
-    try:
-        batch_size, channels, height, width = t.shape
-        for i in range(batch_size):
-            img = np.squeeze(t[i])
-    except Exception:
-        channels, height, width = t.shape
-        img = np.squeeze(t)
-
-    # Normalize to 0-255 range and convert to uint8
-    img = ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
-    image = Image.fromarray(img, mode='L')
-    image.save(save_path, format='png')
-
-
-def save_figure(*data, title, xlabel, ylabel, save_path):
-    for d in data:
-        plt.plot(d)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.savefig(save_path)
-
-
-def get_files(dataset_path: str, include_correlated: bool):
-    """
-    Returns a list file paths to each image in the dataset.
-    """
-    files = os.listdir(dataset_path)
-    if include_correlated:
-        files.extend(os.listdir(os.path.join(dataset_path, 'correlated')))
-    files = [file for file in files if file.endswith('.png')]
-    files_complete = [os.path.join(dataset_path, file) for file in files]
-    return files_complete
-
-
-def train_val_test_split(data: list, split_percent: float, shuffle=True):
-    """
-    Creates a train, val, test split.
-
-    Uses `split_percent` as the training data, delegates half of
-    remaining data to validation and the other half to test.
-
-
-    Args:
-        data (list): List of paths to dataset images
-        split_percent (float): _description_
-        shuffle (bool, optional): _description_. Defaults to True.
-
-    Returns:
-        tuple[list, list, list]: train, val and test image paths.
-    """
-    if shuffle:
-        random.shuffle(data)
-    split1 = int(len(data) * split_percent)
-    split2 = int((len(data) - split1) / 2)
-    train = data[: split1]
-    val = data[split1: split1 + split2]
-    test = data[split1 + split2: (split1 + (split2 * 2))]
-    if len(val) == 0 or len(test) == 0:
-        raise ValueError('Not enough data for train/val/test split.\n'
-                         'Try a lower split_percent')
-    return train, val, test
+import torch
+from PIL import Image
 
 
 def set_device(device: str):
@@ -185,6 +59,205 @@ def set_device(device: str):
         return torch.device('cpu')
     else:
         raise ValueError('Device must be \'mps\', \'cuda\' or \'cpu\'.')
+
+
+def get_files(dataset_path: str, include_correlated: bool):
+    """
+    Gets a list of file paths to each image in the dataset.
+
+    Args:
+        dataset_path (str): Path to dataset root.
+        include_correlated (bool): Include images that have been cross correlated.
+
+    Returns:
+        list: List of full paths to image data.
+    """
+    files = os.listdir(dataset_path)
+    if include_correlated:
+        files.extend(os.listdir(os.path.join(dataset_path, 'correlated')))
+    files = [file for file in files if file.endswith('.png')]
+    files_complete = [os.path.join(dataset_path, file) for file in files]
+    return files_complete
+
+
+def train_val_test_split(data: list, split_percent: float, shuffle=True):
+    """
+    Creates a train, val, test split.
+
+    Uses `split_percent` as the training data, delegates half of
+    remaining data to validation and the other half to test.
+
+
+    Args:
+        data (list): List of paths to dataset images
+        split_percent (float): Percent of data used for training
+        shuffle (bool, optional): Shuffle before splitting. Defaults to True.
+
+    Returns:
+        tuple[list, list, list]: train, val and test image paths.
+    """
+    if shuffle:
+        random.shuffle(data)
+    split1 = int(len(data) * split_percent)
+    split2 = int((len(data) - split1) / 2)
+    train = data[: split1]
+    val = data[split1: split1 + split2]
+    test = data[split1 + split2: (split1 + (split2 * 2))]
+    if len(val) == 0 or len(test) == 0:
+        raise ValueError('Not enough data for train/val/test split.\n'
+                         'Try a lower split_percent')
+    return train, val, test
+
+
+def custom_collate(batch):
+    """
+    Defines how data is passed from the Dataset to the DataLoader.
+
+    Args:
+        batch (list): Batch of data.
+
+    Returns:
+        tuple: Data to be used in the training loop.
+    """
+    input_tensors, target_tensors, original_dimensions, padding_coords, image_path = zip(*batch)
+    return (torch.stack(input_tensors), torch.stack(target_tensors),
+            original_dimensions, padding_coords, image_path)
+
+
+def spectrogram_to_audio(spectrogram_path: str, output_path: str, sample_rate):
+    """
+    Recompose audio using magnitude and phase data retained during
+    spectrogram creation.
+
+    Args:
+        spectrogram_path (str): Path to the spectrogram.
+        output_path (str): Where to save the audio.
+        sample_rate (int): Sample rate in Hz.
+    """
+    # open spectrogram
+    spectrogram_img = Image.open(spectrogram_path)
+    spectrogram_arr = np.array(spectrogram_img)
+
+    # get magnitude and phase values
+    root, specs, img_path = spectrogram_path.split('/')
+    params_path = os.path.join(root, specs, 'params', img_path.replace('.png',
+                                                                       '.json'))
+    # with open(params_path, 'r') as f:
+    with gzip.open(params_path, 'wt') as f:  # jsons are zipped
+        params = json.load(f)
+        magnitude_real = np.array(params['magnitude_real'])
+        magnitude_imag = np.array(params['magnitude_imag'])
+        phase_real = np.array(params['phase_real'])
+        phase_imag = np.array(params['phase_imag'])
+
+        # recreate complex numbers
+        magnitude = magnitude_real + 1j * magnitude_imag
+        phase = phase_real + 1j * phase_imag
+
+        s_db = ((spectrogram_arr / 255)
+                * (np.max(magnitude) - np.min(magnitude)) + np.min(magnitude))
+        s = librosa.db_to_amplitude(s_db, ref=np.max(magnitude))
+
+        # shift sine waves
+        stft_matrix = s * phase
+
+        # inverse short term fourier transform
+        y = librosa.istft(stft_matrix,
+                          hop_length=params['hop_length'],
+                          win_length=params['n_fft'],
+                          length=params.get('original_length'))
+
+        # create path
+        file_path = os.path.join(output_path, spectrogram_path.split('/')[2].replace('.png',
+                                                                                     '.wav'))
+        # save audio
+        sf.write(file_path, y, sample_rate)
+
+
+def remove_padding(tensor, original_dimensions, pad_coords: dict, is_target):
+    """
+    Remove the padding value from a tensor.
+
+    Args:
+        tensor (torch.tensor): Image tensor.
+        original_dimensions (tuple): Original width and height of the image.
+        pad_coords (dict): Coordinates where the padding stretched to.
+        is_target (bool): Target's on the right side of the image so sliced differently.
+
+    Returns:
+        torch.tensor: Tensor with the padding value removed from specified coordinates.
+    """
+    try:
+        batch_size, _, h, w = tensor.shape
+        for i in range(batch_size):
+            # crop tensors with channel values
+            orig_h, orig_w = original_dimensions[i]
+            pad_coords = pad_coords[i]
+            if is_target:
+                cropped_tensor = tensor[i:i+1, :, pad_coords['top']:pad_coords['top']
+                                        + orig_h, :w - pad_coords['right']]
+            else:
+                cropped_tensor = tensor[i:i+1, :, pad_coords['top']:pad_coords['top']
+                                        + orig_h, pad_coords['left']:pad_coords['left'] + orig_w]
+            return cropped_tensor
+    except ValueError:
+        # crop tensors without channel values
+        batch_size, h, w = tensor.shape
+        for i in range(batch_size):
+            orig_h, orig_w = original_dimensions[i]
+            pad_coords = pad_coords[i]
+            if is_target:
+                cropped_tensor = tensor[i:i+1, pad_coords['top']:pad_coords['top']
+                                        + orig_h, :w - pad_coords['right']]
+            else:
+                cropped_tensor = tensor[i:i+1, pad_coords['top']:pad_coords['top']
+                                        + orig_h, pad_coords['left']:pad_coords['left'] + orig_w]
+            return cropped_tensor
+
+
+def save_tensor_as_img(tensor, save_path):
+    """
+    Normalise pixel values to 0-255, convert to a PIL image and save.
+
+    Args:
+        tensor (torch.Tensor): Image.
+        save_path (str): Where to save the image. '.png' is not required.
+    """
+    save_path = save_path.replace('.png', '')  # remove .png if it's there
+    t = tensor.cpu().detach().numpy()  # convert tensor to np array
+    # remove dimension values of 1
+    try:
+        batch_size, channels, height, width = t.shape
+        for i in range(batch_size):
+            img = np.squeeze(t[i])
+    except Exception:
+        channels, height, width = t.shape
+        img = np.squeeze(t)
+
+    # Normalize to 0-255 range and convert to uint8
+    img = ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
+    # convert to PIL image and save
+    image = Image.fromarray(img, mode='L')
+    image.save(save_path, format='png')
+
+
+def save_figure(*data, title, xlabel, ylabel, save_path):
+    """
+    Saves data on a matplotlib line graph. Accepts a variable number of data
+    to plot enabling lines on the same graph.
+
+    Args:
+        title (str): Figure title.
+        xlabel (list): Title for x axis.
+        ylabel (list): Title for y axis.
+        save_path (str): Path to save figure.
+    """
+    for d in data:
+        plt.plot(d)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(save_path)
 
 
 class MPSNotAvailable(Exception):
