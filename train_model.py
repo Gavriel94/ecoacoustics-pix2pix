893916103fe747d68f94535af01b1905bd00fc52
Anthony <anthonygavriel@gmail.com>
2024-08-11 23:21:40 +0100

    use config

--- train_model.py
+++ train_model.py
@@ -11,37 +11,25 @@ from pix2pix.custom_dataset import Pix2PixDataset
 from pix2pix.discriminator import Discriminator
 from pix2pix.generator import Generator
 from pix2pix.custom_loss import Pix2PixLoss
-from pix2pix.train import train_cGAN
+from pix2pix.train_cGAN import train_cGAN
 import pix2pix.utilities as utils
-
-DATASET = 'data_test/'
-DEVICE = utils.set_device('mps')
-LEARNING_RATE = 2e-4
-BATCH_SIZE = 2
-NUM_WORKERS = 0
-NUM_EPOCHS = 3
-L1_LAMBDA = 200  # how much weight is given to the combined L1/Intensity loss
-
-# Gradients accumulate (accumulation_steps * batch_size) steps before updating weights
-ACCUMULATION_STEPS = 4
-# epoch where progress information prints
-DISPLAY_EPOCH = 2
+import config
 
 
 def main():
-    train_dataset = Pix2PixDataset(dataset='data_test/train', use_correlated=False)
-    val_dataset = Pix2PixDataset(dataset='data_test/val', use_correlated=False)
+    train_dataset = Pix2PixDataset(dataset=f'{config.DATASET_ROOT}/train', use_correlated=False)
+    val_dataset = Pix2PixDataset(dataset=f'{config.DATASET_ROOT}/val', use_correlated=False)
     # test_dataset = Pix2PixDataset(dataset='data_test/test', use_correlated=False)
 
     train_loader = DataLoader(train_dataset,
-                              batch_size=BATCH_SIZE,
-                              num_workers=NUM_WORKERS,
+                              batch_size=config.BATCH_SIZE,
+                              num_workers=config.NUM_WORKERS,
                               shuffle=True,
                               collate_fn=utils.custom_collate)
 
     val_loader = DataLoader(val_dataset,
-                            batch_size=BATCH_SIZE,
-                            num_workers=NUM_WORKERS,
+                            batch_size=config.BATCH_SIZE,
+                            num_workers=config.NUM_WORKERS,
                             shuffle=True,
                             collate_fn=utils.custom_collate)
 
@@ -51,18 +39,18 @@ def main():
     #                          shuffle=True,
     #                          collate_fn=utils.custom_collate)
 
-    disc = Discriminator(in_ch=1).to(DEVICE)
-    gen = Generator(in_ch=1, features=64).to(DEVICE)
+    disc = Discriminator(in_ch=1).to(config.DEVICE)
+    gen = Generator(in_ch=1, features=64).to(config.DEVICE)
     # setting betas reduce momentum (used in the paper)
-    optim_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-    optim_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+    optim_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
+    optim_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
     bce = nn.BCEWithLogitsLoss()
     l1_loss = Pix2PixLoss(alpha=0.8)
 
     train_cGAN(disc, gen, train_loader, val_loader,
-               optim_disc, optim_gen, l1_loss, L1_LAMBDA,
-               bce, NUM_EPOCHS, DEVICE, save_dir=DATASET,
-               accumulation_steps=ACCUMULATION_STEPS, view_val_epoch=DISPLAY_EPOCH)
+               optim_disc, optim_gen, l1_loss, config.L1_LAMBDA,
+               bce, config.NUM_EPOCHS, config.DEVICE, save_dir=config.DATASET_ROOT,
+               accumulation_steps=config.ACCUMULATION_STEPS, save_images=config.DISPLAY_EPOCH)
 
 
 if __name__ == '__main__':
