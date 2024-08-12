c7a645f0afb79f4b4fb53f31a354dd842548670f
Anthony <anthonygavriel@gmail.com>
2024-08-12 07:25:34 +0100

    get audio and param paths

--- spectrogram_to_audio.py
+++ spectrogram_to_audio.py
@@ -2,6 +2,8 @@
 Script to turn spectrograms from the test set into audio using a Pix2Pix utility function.
 Used to evaluate the cGANs synthesised spectrograms with unseen data.
 """
+import os
+
 from pix2pix import utilities as utils
 import config
 
@@ -13,22 +15,17 @@ def main():
     target_mic = 'SM4'
     target_mic_delim = '-4'
 
-    test_samples = utils.get_test_sample(dataset_root=data,
-                                         raw_data_root=raw_data,
-                                         mic2_name=target_mic,
-                                         mic2_delim=target_mic_delim,
-                                         num_samples=3)
-    print(test_samples)
+    audio_paths, param_paths = utils.get_test_sample(dataset_root=data,
+                                                     raw_data_root=raw_data,
+                                                     mic2_name=target_mic,
+                                                     mic2_delim=target_mic_delim,
+                                                     num_samples=3)
 
-    # output_path = os.path.join('data', 'spectrograms', 'spectrogram_to_audio')
-    # os.makedirs(output_path, exist_ok=True)
-
-    # overwrite = True
-    # if not overwrite:
-    #     if os.path.exists(os.path.join(output_path, filename)):
-    #         raise FileExistsError
-
-    # utils.spectrogram_to_audio(spectrogram_path, output_path, sample_rate=48000)
+    # perform inference with model
+    for audio in audio_paths:
+        for params in param_paths:
+            if os.path.basename(audio).replace('.wav', '') == params.replace('.json.gz', ''):
+                utils.spectrogram_to_audio(audio, params, config.DATASET_ROOT, 'tmp/e', 48000)
 
 
 if __name__ == '__main__':
